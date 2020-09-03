fa3e34d85b467ebf19370ab349be649e91c5bb6f
M-Usamah <mohammad.usamah27@hotmail.com>
2020-09-03 09:43:42 +0500

    even and odd number

--- Lab/Lab02/Lab2_Task2_Evenodd.py
+++ Lab/Lab02/Lab2_Task2_Evenodd.py
@@ -10,7 +10,7 @@ else:
 
 #Taking the range of number from the user to find even and odd using for loop
 
-taking_a_value = int(input("Enter a value: "))
+# taking_a_value = int(input("Enter a value: "))
 upperlimit = int(input("Enter Upperlimit: "))
 lowerlimit = int(input("Enter Lowerlimit: "))
 for eachnumber in range(upperlimit,lowerlimit+1):
@@ -22,14 +22,14 @@ for eachnumber in range(upperlimit,lowerlimit+1):
 
 #Taking the range of number from the user to find even and odd using while loop
 
-taking_a_value = int(input("Enter a value: "))
+# taking_a_value = int(input("Enter a value: "))
 upperlimit = int(input("Enter Upperlimit: "))
 lowerlimit = int(input("Enter Lowerlimit: "))
 i = upperlimit
 while (i <= lowerlimit):
     modevalue = eachnumber % 2
     if modevalue == 0:
-        print(eachnumber, "is an even")
+        print(i, "is an even")
     else:
-        print(eachnumber, "is an odd")
+        print(i, "is an odd")
     i=i+1
