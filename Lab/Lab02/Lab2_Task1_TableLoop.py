6a525421b134021bbf316c03f07ade58b7462ebb
M-Usamah <mohammad.usamah27@hotmail.com>
2020-09-03 08:49:28 +0500

    Upper and lower limit

--- Lab/Lab02/Lab2_Task1_TableLoop.py
+++ Lab/Lab02/Lab2_Task1_TableLoop.py
@@ -1,17 +1,19 @@
 #Print table using while loop
 
-i = 1
+
 num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
-while (i <= num2):
+num2 = int(input("Enter a upper limit: "))
+num3 = int(input("Enter a lower limit: "))
+i = num2
+while (i <= num3):
     print (i,"*",num1,"=",i*num1)
     i=i+1
 print("=======================================================")
 #Printing table using for loop
 
 num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
-
-for i in range(0, num2+1):
+num2 = int(input("Enter a upper limit: "))
+num3 = int(input("Enter a lower limit: "))
+for i in range(num2, num3+1):
     print (i,"*",num1,"=",i*num1)
 print("=======================================================")
