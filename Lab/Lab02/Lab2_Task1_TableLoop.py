83c66350455e40172c781c43d78b8cbd82345db3
M-Usamah <mohammad.usamah27@hotmail.com>
2020-09-03 08:35:02 +0500

    Done

--- Lab/Lab02/Lab2_Task1_TableLoop.py
+++ Lab/Lab02/Lab2_Task1_TableLoop.py
@@ -1,6 +1,14 @@
 #Print table using while loop
-
-
-
+i = 1
+num1 = int(input("Enter a number: "))
+while (i <= 10):
+    print (i,"*",num1,"=",i*num1)
+    i=i+1
+print("=======================================================")
 #Printing table using for loop
 
+num1 = int(input("Enter a number: "))
+
+for i in range(0, 11):
+    print (i,"*",num1,"=",i*num1)
+print("=======================================================")
