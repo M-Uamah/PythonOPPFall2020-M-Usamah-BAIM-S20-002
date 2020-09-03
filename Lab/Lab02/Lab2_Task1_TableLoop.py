6cae95ec4c6362bdb552ed0d8b83c6003fe45576
M-Usamah <mohammad.usamah27@hotmail.com>
2020-09-03 08:42:50 +0500

    Getting both values

--- Lab/Lab02/Lab2_Task1_TableLoop.py
+++ Lab/Lab02/Lab2_Task1_TableLoop.py
@@ -1,14 +1,17 @@
 #Print table using while loop
+
 i = 1
 num1 = int(input("Enter a number: "))
-while (i <= 10):
+num2 = int(input("Enter a number: "))
+while (i <= num2):
     print (i,"*",num1,"=",i*num1)
     i=i+1
 print("=======================================================")
 #Printing table using for loop
 
 num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
 
-for i in range(0, 11):
+for i in range(0, num2+1):
     print (i,"*",num1,"=",i*num1)
 print("=======================================================")
