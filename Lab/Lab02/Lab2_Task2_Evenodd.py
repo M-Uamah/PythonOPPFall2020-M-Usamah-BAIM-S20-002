88864fb3c91afb8907c1bbf821e311e2850ba1c4
M-Usamah <mohammad.usamah27@hotmail.com>
2020-09-03 10:41:45 +0500

    done

--- Lab/Lab02/Lab2_Task2_Evenodd.py
+++ Lab/Lab02/Lab2_Task2_Evenodd.py
@@ -1,19 +1,19 @@
 #Taking number from user and check it is even or odd
 
-enternumber = int(input("Enter a value: "))
-modevalue = enternumber % 2
+enterNumber = int(input("Enter a value: "))
+modevalue = enterNumber % 2
 if modevalue == 0:
-    print(enternumber,"is an even value")
+    print(enterNumber,"is an even value")
 else:
-    print(enternumber,"is a odd value")
+    print(enterNumber,"is a odd value")
 
 
 #Taking the range of number from the user to find even and odd using for loop
 
 # taking_a_value = int(input("Enter a value: "))
-upperlimit = int(input("Enter Upperlimit: "))
-lowerlimit = int(input("Enter Lowerlimit: "))
-for eachnumber in range(upperlimit,lowerlimit+1):
+lowerLimit = int(input("Enter starting value: "))
+upperLimit = int(input("Enter end value: "))
+for eachnumber in range(lowerLimit,upperLimit+1):
     modevalue = eachnumber % 2
     if modevalue == 0:
         print(eachnumber, "is an even")
@@ -23,41 +23,43 @@ for eachnumber in range(upperlimit,lowerlimit+1):
 #Taking the range of number from the user to find even and odd using while loop
 
 # taking_a_value = int(input("Enter a value: "))
-upperlimit = int(input("Enter Upperlimit: "))
-lowerlimit = int(input("Enter Lowerlimit: "))
+lowerLimit = int(input("Enter starting value: "))
+upperLimit = int(input("Enter end value: "))
 
-while (upperlimit <= lowerlimit):
-    modevalue = upperlimit % 2
+while (lowerLimit <= upperLimit):
+    modevalue = lowerLimit % 2
     if modevalue == 0:
-        print(upperlimit, "is an even")
+        print(lowerLimit, "is an even")
     else:
-        print(upperlimit, "is an odd")
-    upperlimit=upperlimit+1
+        print(lowerLimit, "is an odd")
+    lowerLimit=lowerLimit+1
 
 #Finding totel number of odd and even in a range using for loop
-upperlimit = int(input("Enter Upperlimit: "))
-lowerlimit = int(input("Enter Lowerlimit: "))
+lowerLimit = int(input("Enter starting value: "))
+upperLimit = int(input("Enter end value: "))
 evennumber = 0
 oddnumber = 0
-for eachnumber in range (upperlimit, lowerlimit+1):
+for eachnumber in range (lowerLimit, upperLimit+1):
     modevalue = eachnumber % 2
     if modevalue == 0:
         evennumber = evennumber + 1
     else:
         oddnumber = oddnumber + 1
 print ("Total number of even number is = ",evennumber)
-print ("Total number of even number is = ",oddnumber)
+print ("Total number of odd number is = ",oddnumber)
 
 # Finding totel number of odd and even in a range using while loop
-upperlimit = int(input("Enter Upperlimit: "))
-lowerlimit = int(input("Enter Lowerlimit: "))
+lowerLimit = int(input("Enter starting value: "))
+upperLimit = int(input("Enter end value: "))
 evennumber = 0
 oddnumber = 0
-while (upperlimit <= lowerlimit):
-    # modevalue = upperlimit % 2
-    if upperlimit%2 == 0:
+while (lowerLimit <= upperLimit):
+    modeValue = lowerLimit % 2
+    if modeValue== 0:
         evennumber = evennumber + 1
     else:
         oddnumber = oddnumber + 1
+
+    lowerLimit = lowerLimit + 1
 print ("Total number of even number is = ",evennumber)
-print ("Total number of even number is = ",oddnumber)
+print ("Total number of odd number is = ",oddnumber)
